43f0617cbb39d8709a9d8793a256d9d260af7ba4
zhong zichang <zhongzichang@gmail.com>
2014-07-16 18:20:23 +0800

    init commit

---
+++ src/main/scala/bootstrap/Boot.scala
@@ -0,0 +1,19 @@
+package bootstrap.liftweb
+
+import net.liftweb.http.{Html5Properties, LiftRules, Req}
+import net.liftweb.sitemap.{Menu, SiteMap}
+
+class Boot {
+  def boot{
+    // where to search snippet
+    LiftRules.addToPackages("com.zhongzichang.yuyue")
+    // build sitemap
+    def sitemap(): SiteMap = SiteMap(
+      Menu.i("Home") / "index"
+    )
+
+    // use html5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+  }
+}
